1f8eb5f7f6e3d5dae6ab86d886b95ba5e6fbb254
Happiness549 <mhlongohappiness880@gmail.com>
2025-03-13 16:19:56 +0200

    Added divs using javascript and added a function that changes the background color

---
+++ script.js
@@ -0,0 +1,25 @@
+const container = document.querySelector("#container");
+
+function getRandomColor () {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for(let i = 0; i< 6; i++) 
+        {
+            color += 
+            letters[Math.floor(Math.random()*16)];
+        }
+        return color;
+    }
+     for (let i = 0; i< 16 * 16; i++) {
+        
+    const div = document.createElement("div");
+    div.classList.add("box");
+    
+
+    div.addEventListener(`mouseenter`, () => {
+        div.style.backgroundColor = getRandomColor ();
+    });
+    
+    container.appendChild(div);
+
+}
